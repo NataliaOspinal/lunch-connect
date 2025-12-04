f58d4289a29184bb29254eaf7b87f45f73bc2134
NataliaOspinal <ospinalnatalia@gmail.com>
2025-12-04 14:41:01 -0500

    Update Perfil.jsx

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import InputField from '../components/ui/InputField';
+import { Link, useNavigate } from 'react-router-dom'; // <--- IMPORTANTE
 import { logoutUser } from '../services/authService';
 
 const Perfil = () => {

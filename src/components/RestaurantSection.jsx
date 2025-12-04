dae25deac95607188de7f7206239544b9282fa6f
NataliaOspinal <226923708+NataliaOspinal@users.noreply.github.com>
2025-12-04 09:43:37 -0500

    Corregir Restaurant

--- src/components/RestaurantSection.jsx
+++ src/components/RestaurantSection.jsx
@@ -38,11 +38,6 @@ const CreateGroup = ({ onClose, restaurante }) => {
     
     return coincideBusqueda && noEstaAgregado && cumpleFiltroAmigos;
   });
-  // Filtrar usuarios según la búsqueda (sin filtro por soloAmigos)
-  const usuariosFiltrados = listaAmigos.filter(usuario =>
-    usuario.nombre.toLowerCase().includes(busqueda.toLowerCase()) &&
-    !invitados.find(inv => inv.id === usuario.id)
-  );
 
   const agregarInvitado = (usuario) => {
     if (!capacidad || invitados.length < parseInt(capacidad) - 1) {
